d0187a3c916d24dc4f5df35d336f5b49de059017
Sudhanshu-S3 <sudhanshu.person.1@gmail.com>
2026-07-23 22:32:18 +0530

    add clang format

--- dsa/graph/section_1/102_traversal/1_DFS.cpp
+++ dsa/graph/section_1/102_traversal/1_DFS.cpp
@@ -1,7 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-
 
 /*
 
@@ -11,14 +10,13 @@ using namespace std;
 
 */
 
-// 1. DFS 
+// 1. DFS
 
-//Here i will implement both the recusive DFS and the Iterative DFS
+// Here i will implement both the recusive DFS and the Iterative DFS
 
+// Recursive DFS
 
-// Recursive DFS 
-
-// STEPs : 
+// STEPs :
 
 /*
 1. Think about : we have a start node.
@@ -28,50 +26,52 @@ using namespace std;
 
 */
 
-void DFSre(int node, vector<vector<int>>& adj , vector<bool>& visited){
-
+void DFSre(int                  node,
+           vector<vector<int>>& adj,
+           vector<bool>&        visited)
+{
     visited[node] = true;
 
-    cout<<node<<" ";
+    cout << node << " ";
 
-    for(auto u: adj[node]){
-
-        if(!visited[u]){
-            
+    for (auto u : adj[node])
+    {
+        if (!visited[u])
+        {
             DFSre(u, adj, visited);
         }
     }
 }
 
-
 // Iterative DFS using STACK
 
+void DFSit(int                  node,
+           vector<vector<int>>& graph)
+{
+    // 1. Step 1 : Creating Stack & visited Array.
 
-void DFSit(int node, vector<vector<int>>& graph ){
-
-
-    //1. Step 1 : Creating Stack & visited Array.
-
-    stack<int> st;
+    stack<int>   st;
     vector<bool> visited(node, false);
 
     // 2. Step 2 : Inserting the First node is stack;
 
     st.push(node);
 
-    while(!st.empty()){
+    while (!st.empty())
+    {
         int current = st.top();
         st.pop();
 
-        if(!visited[current]){
-
-            visited[current] = true; // Mark Visited
-            cout<<current<<" ";
+        if (!visited[current])
+        {
+            visited[current] = true;  // Mark Visited
+            cout << current << " ";
 
             // 3. Checking neighbors which are not visited.
-            for(auto u : graph[current]){
-
-                if( !visited[u] ){
+            for (auto u : graph[current])
+            {
+                if (!visited[u])
+                {
                     visited[u] = true;
                     st.push(u);
                 }
@@ -80,7 +80,6 @@ void DFSit(int node, vector<vector<int>>& graph ){
     }
 }
 
-
 // Recursive DFS Extra Practice :
 
 // 1. Undirected graph with Adjacency list.
@@ -88,34 +87,38 @@ void DFSit(int node, vector<vector<int>>& graph ){
 // 3. graph with Adj Matrix.
 // 4. Disconnected Graph.
 
-
 // 1. Undirected graph with Adj list
-void DFSug(int node , vector<vector<int>>& adj , vector<int>& visited){
-
+void DFSug(int                  node,
+           vector<vector<int>>& adj,
+           vector<int>&         visited)
+{
     visited[node] = true;
-    cout<<node<<" ";
+    cout << node << " ";
 
-    for(auto u : adj[node]){
-
-        if(!visited[u]){
-            DFSug(u,adj,visited);
+    for (auto u : adj[node])
+    {
+        if (!visited[u])
+        {
+            DFSug(u, adj, visited);
         }
     }
-
 }
 
 //  2. Direct graph with Adj List
 
-//Code would be same the adj list make all difference.
+// Code would be same the adj list make all difference.
 
-void DFSdg(int node, vector<vector<int>>& adj , vector<int>& visited){
-
+void DFSdg(int                  node,
+           vector<vector<int>>& adj,
+           vector<int>&         visited)
+{
     visited[node] = true;
-    cout<<node<<" ";
+    cout << node << " ";
 
-    for(auto u: adj[node]){
-
-        if(!visited[u]){
+    for (auto u : adj[node])
+    {
+        if (!visited[u])
+        {
             DFSdg(u, adj, visited);
         }
     }
@@ -123,21 +126,26 @@ void DFSdg(int node, vector<vector<int>>& adj , vector<int>& visited){
 
 // 3. Graph with adj matrix
 
-void DFSam(int startNode, vector<vector<int>>& adjMatrix,vector<int>& visited , int Nodes){
-    
-    //1. Mark the current Node as Visited
+void DFSam(int                  startNode,
+           vector<vector<int>>& adjMatrix,
+           vector<int>&         visited,
+           int                  Nodes)
+{
+    // 1. Mark the current Node as Visited
     visited[startNode] = true;
-    cout<<startNode<<" ";
+    cout << startNode << " ";
 
-    for(int i=0 ; i <Nodes ; i++){
+    for (int i = 0; i < Nodes; i++)
+    {
         // 2. Check for Neighbor or not , and then Unvisited Neighbor or not.
-        if(!visited[i] && adjMatrix[startNode][i] == 1){
+        if (!visited[i] && adjMatrix[startNode][i] == 1)
+        {
             DFSam(i, adjMatrix, visited, Nodes);
         }
     }
 }
 
-// 4.  Disconnect Graph 
+// 4.  Disconnect Graph
 
 // DFS function will remain unchange.
 
@@ -155,7 +163,7 @@ for(int i= 0 ;i < Nodes ; i++){
 
 */
 
-int main(){
-
+int main()
+{
     return 0;
 }
--- dsa/graph/section_1/102_traversal/2_BFS.cpp
+++ dsa/graph/section_1/102_traversal/2_BFS.cpp
@@ -1,26 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-//BFS Iterative approach using Queue
+// BFS Iterative approach using Queue
 
-void BFS(int node, vector<vector<int>>& adj , vector<int>& visited){
+void BFS(int                  node,
+         vector<vector<int>>& adj,
+         vector<int>&         visited)
+{
     queue<int> q;
     visited[node] = true;
     q.push(node);
 
-    while(!q.empty()){
+    while (!q.empty())
+    {
         int current = q.front();
         q.pop();
-   
-        cout<< current<< " ";
 
-            for(auto u : adj[current]){
-                
-                if(!visited[u]){
-                    visited[u] = true;
-                    q.push(u);
-                }
+        cout << current << " ";
+
+        for (auto u : adj[current])
+        {
+            if (!visited[u])
+            {
+                visited[u] = true;
+                q.push(u);
             }
+        }
     }
 }
